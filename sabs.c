6d31cfb8b7944549124b83385c8b5f8d788a717f
Eliandra Neto <eneto@c1r4s3.42luanda.com>
2024-07-29 10:56:46 +0100

    Update

--- sabs.c
+++ sabs.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sab.c                                              :+:      :+:    :+:   */
+/*   sabs.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eneto <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 17:18:59 by eneto             #+#    #+#             */
-/*   Updated: 2024/07/26 10:16:10 by eneto            ###   ########.fr       */
+/*   Updated: 2024/07/29 10:49:58 by eneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ void	sa(t_list **a)
 	write(1, "sa\n", 1);
 }
 
-void	sb(t_list *b)
+void	sb(t_list **b)
 {
 	t_list	*prim;
 	t_list	*sec;
-	
+
 	if (*b == NULL || (*b)-> == NULL)
 		return ;
 	prim = *b;
@@ -40,4 +40,23 @@ void	sb(t_list *b)
 	sec->next = prim;
 	*b = sec;
 	write(1, "sb\n", 1);
+}
+
+void	ss(t_list **a, t_list **b)
+{
+	t_list	*prim;
+	t_list	*sec;
+
+	if (*a == NULL || *a->next == NULL)
+		return ;
+	prim = *a;
+	sec = (*a)->next;
+	prim->netx = sec->next;
+	sec->net = prim->next;
+	if (*b == NULL || *b->next == NULL)
+		return ;
+	prim = *b;
+	sec = (*b)->next;
+	prim->next = sec->next;
+	sec->next = prim->next;
 }
